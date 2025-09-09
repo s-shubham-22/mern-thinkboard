6b64d6d571156091bdae8d110053aea0dc9327fa
Shubham Sareliya <shubham.sareliya@unitrix.io>
2025-09-09 23:18:27 +0530

    Add thinkboard package and link frontend/backend dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path'
 
 import router from './routes/index.js'
 import { connectDB } from './config/db.js'
@@ -10,14 +11,25 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-}))
+if (!process.env.NODE_ENV !== 'production') {
+  app.use(cors({
+    origin: 'http://localhost:5173',
+  }))
+}
 app.use(express.json())
 app.use(rateLimiter)
 
 app.use('/api', router)
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve static files from the React frontend app
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
+  })
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
